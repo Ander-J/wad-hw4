59e941ac1952573c7095ae3224267eb1179e6f85
Ander-J <anderjors@gmail.com>
2019-12-15 22:11:13 +0200

    Task 1 complete correctly

--- tests/unit/task1.spec.js
+++ tests/unit/task1.spec.js
@@ -2,9 +2,9 @@ import { mount } from '@vue/test-utils'
 import List from "../../src/components/List";
 
 describe('Task1', () => {
-    const list = mount(List);
+    const wrapper = mount(List);
 
     it('Check if list is empty', () => {
-        expect(Array.isArray(list) && list.length).toBe(false)
+        expect(wrapper.html()).toContain("Add your first Todo task")
     })
 });
